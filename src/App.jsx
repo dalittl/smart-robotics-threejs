612dc89eab744e2dc6bcbdb7d2e497f7773234b7
dalittl <dalittl@users.noreply.github.com>
2025-10-11 20:31:13 -0400

    domain push

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,12 @@ import React from 'react'
 export default function App() {
   return (
     <div className="site blank-page">
-      <header className="floating-header">
-        <div className="brand">
-          <img src="/apolloeve.PNG" alt="Apollo Eve" className="logo" />
-        </div>
-      </header>
-
       {/* Hero center headline */}
       <main className="hero-center">
-        <h1>We build robots!</h1>
+        <div className="hero-inner">
+          <h1>We build robots!</h1>
+          <p className="hero-sublabel">coming soon 2026....</p>
+        </div>
       </main>
     </div>
   )
